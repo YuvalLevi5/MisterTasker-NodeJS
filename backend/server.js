a366450e6e1fef688df925ac4efd10f7fe6cc526
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-08-30 18:18:21 +0300

    deploy

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+const logger = require('./services/logger.service');
+const socketService = require('./services/socket.service');
+
+const taskRoutes = require('./api/task/task.routes');
+
+const app = express();
+const http = require('http').createServer(app);
+
+// const {runWorker} = require('./services/tasker.mngr');
+app.use(express.json()); // create the req.body object - from json
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false})); // create the req.body object
+
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+  ],
+  credentials: true,
+};
+// app.use(cors(corsOptions));
+if (process.env.NODE_ENV === 'production') {
+  console.log("production")
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  console.log("DEV")
+  const corsOptions = {
+      origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080',
+          'http://localhost:8080', 'http://127.0.0.1:3000',
+          'http://localhost:3000', 'http://127.0.0.1:3030',
+          'http://localhost:3030', 'http://localhost:5173'],
+      credentials: true
+  }
+  app.use(cors(corsOptions))
+  
+}
+
+
+socketService.init(http, corsOptions);
+
+app.use('/api/task', taskRoutes);
+
+const port = process.env.PORT || 3030;
+http.listen(port, () => {
+  logger.info('Server is running on port: ' + port);
+});
---
+++ backend/services/db.service.js
@@ -0,0 +1,41 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+// Connection URL
+
+const url = 'mongodb+srv://yuvallevi5:21227159@cluster0.xabqg.mongodb.net/?retryWrites=true&w=majority'
+
+// Database Name
+const dbName = 'tasks_db';
+
+var dbConn = null;
+
+async function connect() {
+    if (dbConn) return dbConn;
+    try {
+        const client = await MongoClient.connect(url, { useUnifiedTopology: true }, { useNewUrlParser: true });
+        const db = client.db(dbName);
+        dbConn = db;
+        console.log('DB connected!')
+        return db;
+    } catch (err) {
+        console.log('Cannot Connect to DB', err)
+        throw err;
+    }
+}
+
+
+async function getCollection(collectionName) {
+    const db = await connect()
+    return db.collection(collectionName);
+}
+
+function toObjectId(id) {
+    return new ObjectId(id)
+}
+
+module.exports = {
+    toObjectId,
+    getCollection
+}
